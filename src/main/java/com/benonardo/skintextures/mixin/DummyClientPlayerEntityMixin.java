9a44bb256616d6d70ada227afb6ff24475164608
Benonardo <75318636+Benonardo@users.noreply.github.com>
2023-04-01 11:40:23 +0200

    Added support for "Show Me Your Skin!"

--- src/main/java/com/benonardo/skintextures/mixin/DummyClientPlayerEntityMixin.java
+++ src/main/java/com/benonardo/skintextures/mixin/DummyClientPlayerEntityMixin.java
@@ -1,30 +1,28 @@
 package com.benonardo.skintextures.mixin;
 
-import com.benonardo.skintextures.SkinTexturesUtil;
 import com.mojang.authlib.GameProfile;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import nl.enjarai.showmeyourskin.client.cursed.DummyClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AbstractClientPlayerEntity.class)
-public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
+@Pseudo
+@Mixin(DummyClientPlayerEntity.class)
+public abstract class DummyClientPlayerEntityMixin extends AbstractClientPlayerEntityMixin {
 
-    public AbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+    public DummyClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
     }
 
+    @Override
     @Inject(method = "getSkinTexture", at = @At("HEAD"), cancellable = true)
     protected void useResourcePackSkinIfPresent(CallbackInfoReturnable<Identifier> cir) {
-        var id = SkinTexturesUtil.getResourcePackSkin(this.getGameProfile());
-        if (id != null) {
-            cir.setReturnValue(id);
-        }
+        super.useResourcePackSkinIfPresent(cir);
     }
 
 }
